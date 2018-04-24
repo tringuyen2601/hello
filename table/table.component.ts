2189ae151c6b26c0e8815b635b7092ad2ccd0a11
FIXED-TERM Nguyen Trong Tri (RBVH/ETM21) <unt81hc@bosch.com>
2018-04-24 17:49:52 +0700

    hol

---
+++ table/table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
